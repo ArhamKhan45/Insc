18d76428eb724596a9ca064e2b289c943220d531
Arham <karham45@gmail.com>
2023-10-30 16:09:44 +0530

    first commit

---
+++ Script.js
@@ -0,0 +1,8 @@
+function open_form() {
+  document.getElementById("myform").style.display = "block";
+  document.getElementById("send_message_button").style.display = "none";
+}
+function close_form() {
+  document.getElementById("myform").style.display = "none";
+  document.getElementById("send_message_button").style.display = "block";
+}
